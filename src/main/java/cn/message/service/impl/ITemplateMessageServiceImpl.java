3ec2e2bba8d70711ebcaaf031a9764f7a91d5cb0
gaoxigang <gaoxigang@chudaokeji.com>
2017-04-20 17:11:54 +0800

    修改发送模板消息接口

--- src/main/java/cn/message/service/impl/ITemplateMessageServiceImpl.java
+++ src/main/java/cn/message/service/impl/ITemplateMessageServiceImpl.java
@@ -32,22 +32,12 @@ public class ITemplateMessageServiceImpl implements ITemplateMessageService {
 	private IMessageCachedImpl iMessageCached;
 
 	@Override
-	public boolean sendMessage(String openId,String url,Map<String, String> map) {
+	public boolean sendMessage(String openId,String templateId,String url,Map<String, Property> map) {
 		TemplateDataModel model = new TemplateDataModel();
 		model.setTouser(openId);
-		model.setTemplate_id("NPDRVXV-5SA2P2CK_rDeL4xRLg3aR3og1Sq3lcCuxOE");
+		model.setTemplate_id(templateId);
 		model.setUrl(url);
-		Map<String, TemplateDataModel.Property> propertys = new HashMap<String, TemplateDataModel.Property>();
-		for (String key : map.keySet()) {
-			//取出参数中的值 组装property
-			String s = map.get(key);
-			Property property = model.new Property();
-			property.setValue(s);
-			property.setColor("#173177");
-			//加入propertys中
-			propertys.put(key, property);
-		}
-		model.setData(propertys);
+		model.setData(map);
 		String json = WebService4Wechat.sendTemplateMessage(
 				iMessageCached.getAccessToken(),
 				model);
